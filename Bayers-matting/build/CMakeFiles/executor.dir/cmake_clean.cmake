b306c911c0666693d746323c689e7a944b2b1727
lxmcwt <3158904764@qq.com>
2022-11-28 19:20:41 +0800

    贝叶斯抠图算法

---
+++ Bayers-matting/build/CMakeFiles/executor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/executor"
+  "../bin/executor.pdb"
+  "CMakeFiles/executor.dir/src/bayers_matting.cpp.o"
+  "CMakeFiles/executor.dir/src/bayers_matting.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/executor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
